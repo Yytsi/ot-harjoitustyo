53ada0decdf3a90eae63178076729caa4b77f5b2
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2023-04-17 15:35:48 +0300

    kuvat ja hahmojen katsomissuunta

--- src/sprites/monster.py
+++ src/sprites/monster.py
@@ -3,11 +3,30 @@ from load_image import load_image
 
 
 class Monster(pygame.sprite.Sprite):
+    MONSTER_DIRECTION_LEFT = 0
+    MONSTER_DIRECTION_RIGHT = 1
+
     def __init__(self, y=0, x=0, height=64, width=64):
         super().__init__()
 
-        self.image = load_image("monster.png")
-        self.image = pygame.transform.scale(self.image, (height, width))
+        self._images = {
+            "monster_facing_right": load_image("monster_facing_right.png"),
+            "monster_facing_left": load_image("monster_facing_left.png")
+        }
+
+        for key, image in self._images.items():
+            self._images[key] = pygame.transform.scale(image, (height, width))
+
+        self.image = self._images["monster_facing_right"]
         self.rect = self.image.get_rect()
         self.rect.y = y
         self.rect.x = x
+        self.direction = self.MONSTER_DIRECTION_LEFT
+
+    def face_to_direction(self, direction):
+        if self.direction != direction:
+            if direction == self.PLAYER_DIRECTION_RIGHT:
+                self.image = self._images["monster_facing_right"]
+            else:
+                self.image = self._images["monster_facing_left"]
+        self.direction = direction
--- src/sprites/player.py
+++ src/sprites/player.py
@@ -3,6 +3,9 @@ from load_image import load_image
 
 
 class Player(pygame.sprite.Sprite):
+    PLAYER_DIRECTION_LEFT = 0
+    PLAYER_DIRECTION_RIGHT = 1
+
     def __init__(self, y=0, x=0, height=64, width=64):
         super().__init__()
 
@@ -13,18 +16,31 @@ class Player(pygame.sprite.Sprite):
             "player_facing_left": load_image("player_facing_left.png")
         }
 
+        for key, image in self._images.items():
+            self._images[key] = pygame.transform.scale(image, (height, width))
+
         self.image = self._images["player_facing_right"]
-        self.image = pygame.transform.scale(self.image, (height, width))
         self.rect = self.image.get_rect()
         self.rect.y = y
         self.rect.x = x
+        self.direction = self.PLAYER_DIRECTION_RIGHT
 
     def update(self):
         if self.player_moving[0]:
+            self.face_to_direction(self.PLAYER_DIRECTION_LEFT)
             self.rect.x -= 2
         if self.player_moving[1]:
             self.rect.y -= 2
         if self.player_moving[2]:
+            self.face_to_direction(self.PLAYER_DIRECTION_RIGHT)
             self.rect.x += 2
         if self.player_moving[3]:
             self.rect.y += 2
+
+    def face_to_direction(self, direction):
+        if self.direction != direction:
+            if direction == self.PLAYER_DIRECTION_RIGHT:
+                self.image = self._images["player_facing_right"]
+            else:
+                self.image = self._images["player_facing_left"]
+        self.direction = direction
