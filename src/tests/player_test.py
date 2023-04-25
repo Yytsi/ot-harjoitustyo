1da6b7ca14c1a71b2c60fa9478e438d856ee520e
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2023-04-25 20:51:58 +0300

    pelaajatesti

--- src/tests/player_test.py
+++ src/tests/player_test.py
@@ -6,9 +6,11 @@ from play_zone import PlayZone
 
 
 class TestPlayer(unittest.TestCase):
+    def setUp(self):
+        self.zone = PlayZone()
+
     def test_cannot_pass_left_wall(self):
-        zone = PlayZone()
-        zone.player1.player_moving[0] = True
+        self.zone.player1.player_moving[0] = True
         for i in range(1000):
-            zone.update_player_positions()
-        self.assertGreater(zone.player1.rect.x, -1)
+            self.zone.update_player_positions()
+        self.assertGreater(self.zone.player1.rect.x, -1)
